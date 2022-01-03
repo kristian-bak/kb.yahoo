804b0f7fd90b741e8559336276c40a2fd5f9e052
kristian-bak <kristianbak@live.com>
2022-01-03 13:12:49 +0100

    Adding pipe operator

--- dev/dev.R
+++ dev/dev.R
@@ -1,15 +1,26 @@
+
+## Document
 devtools::document()
 
+## Load
 devtools::load_all()
 
+## Add package
 usethis::use_package("dplyr")
 usethis::use_package("quantmod")
 
+## Renv
 renv::status()
 renv::snapshot()
 
+## Run unit tests
 devtools::test()
 
+## See test coverage (alternatively Click Addins -> Calculate package test coverage)
 devtools::test_coverage()
 
+## Bump versionnumber
 usethis::use_version()
+
+## Add pipe operator
+usethis::use_pipe()
