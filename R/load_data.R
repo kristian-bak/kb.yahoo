3acad48f7adac1c1f16c8923c76f4d4065ab7377
kristian-bak <kristianbak@live.com>
2022-01-22 15:02:52 +0100

    Adding FRED option in load_data

--- R/load_data.R
+++ R/load_data.R
@@ -1,5 +1,6 @@
 #' Load data from Yahoo.
 #' @param ticker ticker code from Yahoo.
+#' @param src source name ("yahoo" or "FRED")
 #' @param from load data `from` untill today where the format of `from` is yyyy-mm-dd.
 #' @return A data.frame
 #' @examples
@@ -8,10 +9,10 @@
 #' data
 #' @export
 #'
-load_data <- function(ticker, from = "2014-01-01") {
+load_data <- function(ticker, src = "yahoo", from = "2014-01-01") {
 
   data <- catch_error(
-    quantmod::getSymbols(ticker, auto.assign = FALSE, from = from, src = "yahoo")
+    quantmod::getSymbols(ticker, auto.assign = FALSE, from = from, src = src)
   )
 
   if (!is.null(data$error)) {
@@ -21,10 +22,23 @@ load_data <- function(ticker, from = "2014-01-01") {
   data <- data$value %>%
     as.data.frame() %>%
     dplyr::mutate(Date = rownames(.) %>% as.Date()) %>%
-    dplyr::as_tibble() %>%
-    dplyr::rename_with(.data = ., ~ gsub(paste0(ticker, "."), "", .x)) %>%
-    dplyr::mutate(Change = price_change(Close, digits = 4)) %>%
-    dplyr::relocate(Date, Open, High, Low, Close, Adjusted, Change, Volume)
+    dplyr::as_tibble()
+
+  if (src == "yahoo") {
+
+    data <- data %>%
+      dplyr::rename_with(.data = ., ~ gsub(paste0(ticker, "."), "", .x)) %>%
+      dplyr::mutate(Change = price_change(Close, digits = 4)) %>%
+      dplyr::relocate(Date, Open, High, Low, Close, Adjusted, Change, Volume)
+
+  } else {
+
+    data <- data %>%
+      dplyr::rename_with(~ gsub(ticker, "Close", .x)) %>%
+      dplyr::mutate(Change = price_change(Close, digits = 4)) %>%
+      dplyr::relocate(Date, Close, Change)
+
+  }
 
   return(data)
 
--- tests/testthat/test-load_data.R
+++ tests/testthat/test-load_data.R
@@ -1,4 +1,4 @@
-test_that("Load data works", {
+test_that("Load yahoo data works", {
 
   data <- load_data(ticker = "NOVO-B.CO", from = "2021-01-01")
 
@@ -20,5 +20,24 @@ test_that("Load data works", {
     load_data(ticker = "Unknown", from = "2021-01-01"),
     "Load data failed for Unknown"
   )
+
+})
+
+test_that("Load FRED data works", {
+
+  data <- load_data(ticker = "MORTGAGE30US", src = "FRED")
+
+  cols <- c("Date", "Close", "Change")
+
+  ## Expecting identical column names
+  expect_identical(names(data), cols)
+
+  price <- data %>%
+    dplyr::filter(Date == "2022-01-06") %>%
+    dplyr::pull(Close) %>%
+    round(2)
+
+  ## Expecting a certain closing value in the first row
+  expect_equal(price, 3.22)
 
 })
